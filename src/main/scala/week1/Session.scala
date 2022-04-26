5520ccc108be19b8a2120f2a4c6218e5a165136c
leandro <leandro.moquiuti@gmail.com>
2022-04-26 10:12:54 -0400

    Criação de Blocos

--- src/main/scala/week1/Session.scala
+++ src/main/scala/week1/Session.scala
@@ -3,19 +3,22 @@ package week1
 object Session extends App {
   def abs(x: Double) = if (x < 0) -x else x
 
-  def sqrIter(guess: Double, x: Double): Double =
-    if (isGoodEnough(guess, x)) guess
-    else sqrIter(improve(guess, x), x)
+  def sqr(x: Double) = {
+    def sqrIter(guess: Double): Double =
+      if (isGoodEnough(guess)) guess
+      else sqrIter(improve(guess))
 
-  def isGoodEnough(guess: Double, x: Double) = abs(guess * guess - x) / x < 0.001
+    def isGoodEnough(guess: Double) = abs(guess * guess - x) / x < 0.001
 
-  def improve(guess: Double, x: Double) = (guess + x / guess) / 2
+    def improve(guess: Double) = (guess + x / guess) / 2
 
-  def sqr(x: Double) = sqrIter(1.0, x)
+    println(sqrIter(1.0))
+  }
 
-  println(sqr(0.001))
-  println(sqr(0.1e-20))
-  println(sqr(1.0e20))
-  println(sqr(1.0e50))
-
+  println(sqr(2))
+  println(sqr(4))
+  println(sqr(1e-6))
+  println(sqr(1e60))
 }
+
+

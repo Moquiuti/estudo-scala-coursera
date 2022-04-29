895e20cbafd45c81f2d2d902a217d44760944fb5
leandro <leandro.moquiuti@gmail.com>
2022-04-29 15:37:43 -0400

    week 4

---
+++ src/main/scala/week4/List.scala
@@ -0,0 +1,22 @@
+package week4
+
+import java.util.NoSuchElementException
+
+trait List[T] {
+  def isEmpty: Boolean
+  def head: T
+  def tail: List[T]
+}
+// a diferença entre val e def é que val é avaliada quando o método é inicializado pela primeira vez
+// e def é avaliado cada vez que é referenciado
+class Cons[T](val head: T, val tail: List[T]) extends List[T] {
+  def isEmpty: Boolean = false
+}
+
+class Nil[T] extends List[T] {
+  def isEmpty: Boolean = true
+
+  def head: Nothing = throw new NoSuchElementException("Nil.head")
+
+  def tail: Nothing = throw new NoSuchElementException("Nil.tail")
+}

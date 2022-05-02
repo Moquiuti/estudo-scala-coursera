bfb375165f6bc3515a078575c073913af2ce8011
leandro <leandro.moquiuti@gmail.com>
2022-05-02 08:43:00 -0400

    Functions as Objects

--- src/main/scala/week4/List.scala
+++ src/main/scala/week4/List.scala
@@ -16,8 +16,17 @@ class Cons[T](val head: T, val tail: List[T]) extends List[T] {
 
 class Nil[T] extends List[T] {
   def isEmpty: Boolean = true
-
   def head: Nothing = throw new NoSuchElementException("Nil.head")
-
   def tail: Nothing = throw new NoSuchElementException("Nil.tail")
+}
+
+/**
+ * Define an object List{...} with 3 functions in it so that users can create lists of length 0-2 using syntax
+ * List() //the empty list
+ * List(1) //the list with single element 1.
+ * List(2, 3) // the list with elements 2 and 3
+ * */
+object List {
+  def apply[T](x1: T, x2: T): List[T] = new Cons(x1, new Cons(x2, new Nil))
+  def apply[T]() = new Nil
 }

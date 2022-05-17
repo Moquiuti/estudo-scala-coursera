b164709c20eeea9602a25ffa9bcc75fee99219f3
leandro <leandro.moquiuti@gmail.com>
2022-05-17 12:44:33 -0400

    show

--- src/main/scala/week6/nqueens.scala
+++ src/main/scala/week6/nqueens.scala
@@ -4,7 +4,7 @@ object nqueens extends App {
   def queens(n: Int): Set[List[Int]] = {
     def placeQueens(k: Int): Set[List[Int]] =
       if (k == 0) Set(List())
-      else {
+      else
         for {
           queens <- placeQueens(k - 1)
           col <- 0 until n
@@ -12,7 +12,7 @@ object nqueens extends App {
         } yield col :: queens
         placeQueens(n)
       }
-  }
+
 
   def isSafe(col: Int, queens: List[Int]): Boolean = {
     val row = queens.length
@@ -21,4 +21,12 @@ object nqueens extends App {
       case (r, c) => col != c && math.abs(col - c) != row - r
     }
   }
+
+  def show(queens: List[Int]) = {
+    val lines =
+      for (col <- queens.reverse)
+        yield Vector.fill(queens.length)("* ").updated(col, "X ").mkString
+    "\n" + (lines mkString ("\n"))
+  }
+  print(queens(4) map show)
 }

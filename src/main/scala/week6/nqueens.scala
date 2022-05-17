06003ef83694fccd118283b1fa755e9b868fbb36
leandro <leandro.moquiuti@gmail.com>
2022-05-17 12:39:24 -0400

    isSafe

--- src/main/scala/week6/nqueens.scala
+++ src/main/scala/week6/nqueens.scala
@@ -1,11 +1,11 @@
 package week6
 
-object nqueens extends App{
+object nqueens extends App {
   def queens(n: Int): Set[List[Int]] = {
     def placeQueens(k: Int): Set[List[Int]] =
-      if(k == 0) Set(List())
+      if (k == 0) Set(List())
       else {
-        for{
+        for {
           queens <- placeQueens(k - 1)
           col <- 0 until n
           if isSafe(col, queens)
@@ -14,4 +14,11 @@ object nqueens extends App{
       }
   }
 
+  def isSafe(col: Int, queens: List[Int]): Boolean = {
+    val row = queens.length
+    val queensWithRow = (row - 1 to 0 by -1) zip queens
+    queensWithRow forall {
+      case (r, c) => col != c && math.abs(col - c) != row - r
+    }
+  }
 }

65ae25ca96dd1f17dad3352ebdceeb2c13e8cb4a
leandro <leandro.moquiuti@gmail.com>
2022-05-17 12:35:29 -0400

    placeQueens

--- src/main/scala/week6/nqueens.scala
+++ src/main/scala/week6/nqueens.scala
@@ -1,6 +1,17 @@
 package week6
 
 object nqueens extends App{
-
+  def queens(n: Int): Set[List[Int]] = {
+    def placeQueens(k: Int): Set[List[Int]] =
+      if(k == 0) Set(List())
+      else {
+        for{
+          queens <- placeQueens(k - 1)
+          col <- 0 until n
+          if isSafe(col, queens)
+        } yield col :: queens
+        placeQueens(n)
+      }
+  }
 
 }
